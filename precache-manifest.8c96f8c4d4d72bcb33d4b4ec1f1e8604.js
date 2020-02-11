53e2a37325e7c0d190172f5ff93ea7d84414be44
Artem2313 <huselnykov@gmail.com>
2020-02-11 17:21:15 +0200

    Updates

---
+++ precache-manifest.8c96f8c4d4d72bcb33d4b4ec1f1e8604.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd9fc0033223923e7bbbb27e857fcb05",
+    "url": "/goit-react-hw-05-phonebook/index.html"
+  },
+  {
+    "revision": "ca5f0731fa3a02d9fa3c",
+    "url": "/goit-react-hw-05-phonebook/static/css/main.158e36de.chunk.css"
+  },
+  {
+    "revision": "bbb63e5c6476f002684e",
+    "url": "/goit-react-hw-05-phonebook/static/js/2.e8b562cc.chunk.js"
+  },
+  {
+    "revision": "ca5f0731fa3a02d9fa3c",
+    "url": "/goit-react-hw-05-phonebook/static/js/main.acab79ec.chunk.js"
+  },
+  {
+    "revision": "ba7eecc1a22cea7b0be0",
+    "url": "/goit-react-hw-05-phonebook/static/js/runtime-main.8b23889c.js"
+  }
+]);
